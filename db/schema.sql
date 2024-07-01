047686310a3c4662baf361d849523e03cc44022e
brenda-soto <bsoto@pursuit.org>
2024-07-01 11:55:03 -0400

    Updated schema to include references

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,18 @@
 -- db/schema.sql
 DROP DATABASE IF EXISTS global_agent;
-CREATE DATABASE debug_arena;
+CREATE DATABASE global_agent;
 
 \c global_agent;
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     uid VARCHAR(255),
-    email UNIQUE VARCHAR(100),
-    username UNIQUE VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    username VARCHAR(100) UNIQUE NOT NULL,
     dob DATE,
     photo VARCHAR(200),
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-)
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
 
 CREATE TABLE stats (
     id SERIAL PRIMARY KEY,
@@ -21,7 +21,7 @@ CREATE TABLE stats (
     questions_correct INTEGER NOT NULL,
     questions_wrong INTEGER NOT NULL,
     user_id INTEGER NOT NULL
-)
+);
 
 CREATE TABLE badges (
     id SERIAL PRIMARY KEY,
@@ -29,23 +29,23 @@ CREATE TABLE badges (
     image TEXT NOT NULL,
     description VARCHAR(200),
     xp_required INTEGER NOT NULL
-)
+);
 
 CREATE TABLE user_badges (
     id SERIAL PRIMARY KEY,
-    badge_id INTEGER NOT NULL,
-    user_id INTEGER NOT NULL
-)
+    badge_id INTEGER NOT NULL REFERENCES badges(id),
+    user_id INTEGER NOT NULL REFERENCES users(id)
+);
 
 CREATE TABLE countries (
     id SERIAL PRIMARY KEY,
     image TEXT NOT NULL,
     name VARCHAR(30)
-)
+);
 
 CREATE TABLE visited_countries (
    id SERIAL PRIMARY KEY,
-   countries_id INTEGER NOT NULL,
-   user_id INTEGER NOT NULL
+   countries_id INTEGER NOT NULL REFERENCES countries(id),
+   user_id INTEGER NOT NULL REFERENCES users(id)
 );
 
