debf3cd4e7eeeb4206993941e2fb71108bb3640c
AnitaOwen <anitaowen@pursuit.org>
2024-07-02 16:32:45 -0400

    created GET for case files by country

--- controllers/countriesController.js
+++ controllers/countriesController.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const countries = express.Router()
 
-const {getAllCountries} = require("../queries/countries")
+const {getAllCountries, getCaseFilesByCountry} = require("../queries/countries")
 
 //INDEX http://localhost:3003/api/countries
 countries.get('/', async (req,res) => {
@@ -11,6 +11,19 @@ countries.get('/', async (req,res) => {
         res.status(200).json(allCountries);
     } else {
         res.status(500).json({ error: "Error fetching Countries"})
+    }
+});
+
+
+//CASE FILES INDEX http://localhost:3003/api/countries/1/case_files
+countries.get('/:countries_id/case_files', async (req,res) => {
+    const { countries_id } = req.params
+    const allCaseFilesByCountry = await getCaseFilesByCountry(countries_id);
+
+    if(allCaseFilesByCountry[0]){
+        res.status(200).json(allCaseFilesByCountry);
+    } else {
+        res.status(500).json({ error: "Error fetching case files"})
     }
 });
 
