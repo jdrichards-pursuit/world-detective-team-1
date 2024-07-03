df11a8313ebb794f9bd5862714d8744f6e82fd2c
AnitaOwen <anitaowen@pursuit.org>
2024-07-03 15:39:50 -0400

    edit create new stats

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -6,7 +6,6 @@ const {getStatsByUserId, updateUserStats, createUserStats} = require("../queries
 stats.get('/:user_id', async (req,res) => {
     const { user_id } = req.params
     const userStats = await getStatsByUserId(user_id);
-
     if(userStats[0]){
         res.status(200).json(userStats);
     } else {
@@ -14,8 +13,20 @@ stats.get('/:user_id', async (req,res) => {
     }
 });
 
+
+// POST http://localhost:3003/api/stats/1
+stats.post("/:user_id" ,async (req, res) => {
+  const { user_id } = req.params
+  const newUserStats = await createUserStats(user_id)
+  if (newUserStats.id) {
+    res.status(200).json(newUserStats);
+  } else {
+    res.status(404).json({ error: "User stats not created" });
+  }
+});
+
 // UPDATE http://localhost:3003/api/stats/1
-stats.put("/:user_id" ,async (req, res) => {
+stats.put("/:user_id", async (req, res) => {
     const { user_id } = req.params
     const updatedUserStats = await updateUserStats({user_id, ...req.body});
     if (updatedUserStats.id) {
@@ -24,16 +35,4 @@ stats.put("/:user_id" ,async (req, res) => {
       res.status(404).json({ error: "User stats not updated" });
     }
   });
-
-  // POST http://localhost:3003/api/stats/1
-stats.post("/:user_id" ,async (req, res) => {
-    const { user_id } = req.params
-    const newUserStats = await createUserStats(user_id)
-    if (newUserStats.id) {
-      res.status(200).json(newUserStats);
-    } else {
-      res.status(404).json({ error: "User stats not created" });
-    }
-  });
-
 module.exports = stats
--- queries/stats.js
+++ queries/stats.js
@@ -3,34 +3,15 @@ const db = require("../db/dbConfig");
 
 const getStatsByUserId = async (user_id) => {
     try {
-        const statsByUserId  = await db.any(`SELECT * FROM stats WHERE stats.user_id = $1`, user_id);
+        const statsByUserId  = await db.any(`SELECT * FROM stats WHERE user_id = $1`, user_id);
         return statsByUserId
     } catch (error) {
         return error;
     }
 };
 
-// UPDATE 
-const updateUserStats = async (stats) => {
-    try {
-      const updatedUserStats = await db.one(
-        `UPDATE stats SET xp=$1, games_played=$2, questions_correct=$3, questions_wrong=$4 WHERE user_id=$5 RETURNING *`,
-        [
-          stats.xp,
-          stats.games_played,
-          stats.questions_correct,
-          stats.questions_wrong,
-          stats.user_id
-        ]
-      );
-      return updatedUserStats;
-    } catch (error) {
-      return error;
-    }
-  };
-
   // CREATE
-const createUserStats = async (user_id) => {
+const createUserStats = async () => {
     try {
       const newUserStats = await db.one(
         `INSERT INTO stats(user_id) VALUES($1) RETURNING *`, user_id
@@ -41,6 +22,26 @@ const createUserStats = async (user_id) => {
     }
   };
   
+  // UPDATE 
+const updateUserStats = async (stats) => {
+  try {
+    const updatedUserStats = await db.one(
+      `UPDATE stats SET xp=$1, games_played=$2, questions_correct=$3, questions_wrong=$4 WHERE user_id=$5 AND id=$6 RETURNING *`,
+      [
+        stats.xp,
+        stats.games_played,
+        stats.questions_correct,
+        stats.questions_wrong,
+        stats.user_id,
+        stats.id
+      ]
+    );
+    return updatedUserStats;
+  } catch (error) {
+    return error;
+  }
+};
+
 
 
 module.exports = {
