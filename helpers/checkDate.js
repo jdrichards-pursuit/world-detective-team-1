b7e5b6f92820c604238e6e9242b7a16c91039c25
AnitaOwen <anitaowen@pursuit.org>
2024-07-10 11:46:43 -0400

    made helper files for the case files post functions

--- helpers/checkDate.js
+++
@@ -1,9 +0,0 @@
-const checkDate = (article_publish_date) => {
-  const articleDate = new Date(article_publish_date);
-  const currentDate = new Date();
-  const differeneInDatesInMs = currentDate - articleDate;
-  const msToDay = 1000 * 60 * 60 * 24;
-  return (differeneInDatesInMs / msToDay).toFixed(1);
-};
-
-module.exports = checkDate;
